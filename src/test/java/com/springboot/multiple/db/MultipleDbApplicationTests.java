1811681cf0d7b883ea85783d401d9ff05913597b
danosella <danosella96@gmail.com>
2023-07-15 21:51:17 +0700

    configurasi database

---
+++ src/test/java/com/springboot/multiple/db/MultipleDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.multiple.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
